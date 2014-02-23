0396641176be6616d2703f914d83b873f3989627
twlkyao <qishiyao2008@126.com>
2014-02-23 22:51:21 +0800

    Optimize the implementation of ApkValidate.java, still way to go.

--- src/com/twlkyao/androidcloud/ApkValidate.java
+++ src/com/twlkyao/androidcloud/ApkValidate.java
@@ -10,14 +10,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ApkValidate extends Activity {
 
+	private LinearLayout layout;
+	private Button btn_cancel;
+	private Button btn_ok;
+	private TextView tv_message;
+	
 	private ApkOperation apkOperation = new ApkOperation();
 	private String filePath;
 	private String tag = "ApkValidation";
@@ -33,14 +39,70 @@ public class ApkValidate extends Activity {
 			// TODO Auto-generated method stub
 			super.handleMessage(msg);
 			switch(msg.what) {
-			case 1:
-				Toast.makeText(getApplicationContext(),
-						R.string.app_passed,
-						Toast.LENGTH_SHORT).show();
-				finish();
+			case 1: // Validation passed.
+				tv_message.setText(getString(R.string.app_passed));
+				
+				// Need a better way to show the result.
+//				try {
+//					Thread.sleep(10000);
+//					finish();
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+				btn_ok.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						logUtils.d(tag, "btn_ok");
+						finish();
+					}
+				});
+				
+				btn_cancel.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						logUtils.d(tag, "btn_cancel");
+					
+						finish();
+					}
+				});
+				
 				break;
-			case 0:
-				Builder builder = new AlertDialog.Builder(ApkValidate.this);
+			case 0: // Validation failed.
+				tv_message.setText(getString(R.string.app_failed));
+				btn_cancel.setText(getString(R.string.btn_delete));
+				btn_ok.setText(getString(R.string.btn_reserve));
+				btn_ok.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						logUtils.d(tag, "No operation");
+						finish();
+					}
+				});
+				
+				btn_cancel.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						File file = new File(filePath);
+						if(file.exists()) {
+							file.delete();
+						
+							logUtils.d(tag, "File deleted");
+						}
+						finish();
+					}
+				});
+				
+				
+				/*Builder builder = new AlertDialog.Builder(ApkValidate.this);
 				builder.setTitle(R.string.app_validate_title);
 				builder.setIcon(android.R.drawable.ic_dialog_alert);
 				builder.setMessage(R.string.app_failed);
@@ -69,7 +131,7 @@ public class ApkValidate extends Activity {
 				});
 				builder.create();
 				builder.show();
-				break;
+				break;*/
 			default:
 				break;
 			}
@@ -79,18 +141,17 @@ public class ApkValidate extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_apk_validate);
+		
+		setContentView(R.layout.activity_apk_validate); // Set contentView.
 		
 		Bundle bundle = getIntent().getExtras();
 		logUtils.d(tag, filePath);
 		
 		filePath = bundle.getString("filepath");
 		
-		
-		
 		final Message msg = Message.obtain();
 		
-		Thread thread = new Thread(new Runnable() {
+		final Thread thread = new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
@@ -107,5 +168,48 @@ public class ApkValidate extends Activity {
 		
 		thread.start(); // Start the thread.
 		
+		layout=(LinearLayout)findViewById(R.id.apk_validate_layout); // Find the layout.
+		btn_cancel = (Button) findViewById(R.id.btn_cancel);
+		btn_ok = (Button) findViewById(R.id.btn_ok);
+		tv_message = (TextView) findViewById(R.id.tx_message);
+		
+		// Optional
+		layout.setOnClickListener(new View.OnClickListener() { // Set layout click listener.
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(),
+						R.string.click_error,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+				
+		btn_cancel.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				logUtils.d(tag, "Canceled");
+				thread.stop();
+				finish();
+			}
+		});
+				
+		btn_ok.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				logUtils.d(tag, "No operation");
+			}
+		});
 	}
+
+	// Finish this activity when hit other place.
+	public boolean onTouchEvent(MotionEvent event){
+//		finish();
+		return true;
+	}
+	
 }
